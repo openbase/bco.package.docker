53eae963f9e7e4b41bde401d622ba1d3281aee34
Divine Threepwood <divine@openbase.org>
2020-07-23 18:04:12 +0200

    setup sitemap permission for bco user within new module prepare script.

---
+++ device-manager/openhab/bco-module-prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OPENHAB_USER_NAME="openhab"
+
+# Prepare sitemap folder access
+if [ -z ${OPENHAB_GROUP_ID+x} ]; then
+    echo "openhab group id not set! Therefore, sitemap permissions can not be guaranteed.";
+else
+    echo "add bco user to openhab group to guarantee sitemap folder access...";
+
+    # create openhab group within the container
+    groupadd -g ${OPENHAB_GROUP_ID} ${OPENHAB_USER_NAME}
+
+    # register bco user as member of the openhab group
+    usermod -a -G ${OPENHAB_USER_NAME} ${BCO_USER}
+fi
